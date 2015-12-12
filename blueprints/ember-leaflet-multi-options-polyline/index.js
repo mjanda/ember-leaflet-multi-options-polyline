47bfe593cf45d68b6e55e6d78c7dfcc2ef8a9b61
Marek Janda <nyx@nyx.cz>
2015-12-12 23:30:56 +0100

    test

--- blueprints/ember-leaflet-multi-options-polyline/index.js
+++ blueprints/ember-leaflet-multi-options-polyline/index.js
@@ -16,6 +16,6 @@ module.exports = {
   },
 
   afterInstall: function(options) {
-   return this.addBowerPackageToProject('hgoebl/Leaflet.MultiOptionsPolyline', '~0.8.1');
+   return this.addBowerPackageToProject('git@github.com:hgoebl/Leaflet.MultiOptionsPolyline', '~0.8.1');
   }
 };
